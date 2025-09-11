9ac35b9708b25c5003810d152dfa9733497c56f6
Dmitriy Folomkin <dmitriy.folomkin@yandex.ru>
2025-09-11 10:29:13 +0300

    new task

---
+++ src/test/kotlin/TestExample.kt
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import kotlin.math.abs
+import kotlin.math.absoluteValue
+
+class TestExample {
+
+
+    }
